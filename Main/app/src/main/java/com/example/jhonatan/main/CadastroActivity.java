f4966945ccee698d761909e14a3231033479590b
Eduardo Segura <zero.duh@gmail.com>
2017-10-23 22:20:01 -0200

    Correções para apresentação do caso de uso registrar

--- Main/app/src/main/java/com/example/jhonatan/main/CadastroActivity.java
+++ Main/app/src/main/java/com/example/jhonatan/main/CadastroActivity.java
@@ -9,15 +9,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private int editText;
+    private EditText editName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        editName = (EditText)findViewById(R.id.editText);
     }
 
     protected void voltarMain(View view){
@@ -28,14 +31,21 @@ public class CadastroActivity extends AppCompatActivity {
     protected void irMenuPrincipal(View view) throws InterruptedException {
 
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setMessage("Usuário Cadastrado!");
+        builder1.setMessage("Usuário " + editName.getText().toString() +  " Cadastrado!");
         builder1.setTitle("Feito!");
         builder1.setCancelable(false);
+
+        builder1.setCancelable(false)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent2 = new Intent(getApplicationContext(), MenuPrincipalActivity.class);
+                        startActivity(intent2);
+                    }
+                });
+
         AlertDialog alert11 = builder1.create();
         alert11.show();
 
-        Intent intent2 = new Intent(getApplicationContext(), MenuPrincipalActivity.class);
-        startActivity(intent2);
 
         /*CRIAR UM OBJETO PESSOA AQUI COM OS DADOS DOS CAMPOS DE TEXTO (menos data de nascimento) */
 
