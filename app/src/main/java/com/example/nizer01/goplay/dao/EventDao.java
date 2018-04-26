eb7fa9b2b4be163b2a8a0b1b2d220e57183aec4e
zeroduh <zero.duh@gmail.com>
2018-04-26 20:22:01 -0300

    Agora com firebase

--- app/src/main/java/com/example/nizer01/goplay/dao/EventDao.java
+++ app/src/main/java/com/example/nizer01/goplay/dao/EventDao.java
@@ -5,6 +5,8 @@ import android.widget.ArrayAdapter;
 import com.example.nizer01.goplay.domain.Activity;
 import com.example.nizer01.goplay.domain.Event;
 import com.example.nizer01.goplay.domain.Local;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.sql.Timestamp;
 import java.text.DateFormat;
@@ -18,6 +20,11 @@ public class EventDao {
     private static ArrayList<Event> eventList = new ArrayList<>();
 
     public static void createEvent(Event ev){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue(ev);
         eventList.add(ev);
     }
 
